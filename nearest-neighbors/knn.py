83ba85c622bcfe23af948a55bb4f2b73aa788dc5
Sayar1106 <sayarbanerjee@gmail.com>
2023-03-14 00:06:16 -0700

    Working model

--- nearest-neighbors/knn.py
+++ nearest-neighbors/knn.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from abc import ABC, abstractmethod
 from random import random 
+from scipy import stats
 
 class KNN(ABC):
     def __init__(self, k):
@@ -11,6 +13,7 @@ class KNN(ABC):
 
     def fit(self, X, y):
         self.X = X
+        self.y = y
 
     @abstractmethod
     def predict(self, X):
@@ -18,8 +21,15 @@ class KNN(ABC):
 
 class KNNClassifier(KNN):
     def predict(self, X):
-        distance_list = np.linalg.norm(self.X - X)
-        print(distance_list)
+        arr = np.zeros((X.shape[0], self.X.shape[0]))
+        for i in range(X.shape[0]):
+            for j in range(self.X.shape[0]):
+                arr[i, j] = np.linalg.norm(self.X[j, :] - X[i, :])
+        arr_indices = np.argsort(arr)
+        pred_labels = (stats.mode(self.y[item[:self.k]]).mode[0] for item in arr_indices)
+        return list(pred_labels)
+
+        
 
     
 
@@ -27,6 +37,9 @@ if __name__ == "__main__":
     df = load_iris()
     classifier = KNNClassifier(5)
     idx = random()
-    train_X, test_X, y_train, y_test = train_test_split(df, shuffle=True, random_state=4848, test_size=0.2)
-    classifier.fit(train_X)
-    classifier.predict(test_X)
+    train_X, test_X, y_train, y_test = train_test_split(df.data, df.target, shuffle=True, random_state=4848, test_size=0.2)
+    classifier.fit(train_X, y_train)
+    y_pred = classifier.predict(test_X)
+    print(y_test)
+    print(y_pred)
+    # print(f"Accuracy: {accuracy_score(y_pred, y_test)}")
