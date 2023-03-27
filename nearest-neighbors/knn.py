c0661443371d8185371796f08f4699cca9c4748b
Sayar1106 <sayarbanerjee@gmail.com>
2023-03-27 15:27:37 -0700

    Creating simple CLI interface for testing

--- nearest-neighbors/knn.py
+++ nearest-neighbors/knn.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris, load_diabetes
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.metrics import accuracy_score, mean_squared_error, mean_absolute_percentage_error
 from abc import ABC, abstractmethod
-from random import random 
 from scipy import stats
+import click
 
 class KNN(ABC):
     def __init__(self, k):
@@ -26,20 +27,54 @@ class KNNClassifier(KNN):
             for j in range(self.X.shape[0]):
                 arr[i, j] = np.linalg.norm(self.X[j, :] - X[i, :])
         arr_indices = np.argsort(arr)
-        pred_labels = (stats.mode(self.y[item[:self.k]]).mode[0] for item in arr_indices)
-        return list(pred_labels)
+        pred_labels = (stats.mode(self.y[item[:self.k]], keepdims=True).mode[0] for item in arr_indices)
+        return np.array(list(pred_labels))
 
-        
+class KNNRegression(KNN):
+    def predict(self, X):
+        arr = np.zeros((X.shape[0], self.X.shape[0]))
+        for i in range(X.shape[0]):
+            for j in range(self.X.shape[0]):
+                arr[i, j] = np.linalg.norm(self.X[j, :] - X[i, :])
+        arr_indices = np.argsort(arr)
+        pred_vals = (np.mean(self.y[item[:self.k]]) for item in arr_indices)
+        return np.array(list(pred_vals))
 
-    
 
-if __name__ == "__main__":
+@click.command()
+@click.option("--k", default=5)
+@click.option("--random_no", default=4848)
+@click.option("--t_size", default=0.2)
+def main(k, random_no, t_size):
+    print("Testing on iris data....")
     df = load_iris()
-    classifier = KNNClassifier(5)
-    idx = random()
-    train_X, test_X, y_train, y_test = train_test_split(df.data, df.target, shuffle=True, random_state=4848, test_size=0.2)
+    classifier = KNNClassifier(k)
+    train_X, test_X, y_train, y_test = train_test_split(df.data, df.target, shuffle=True, random_state=random_no, stratify=df.target, test_size=t_size)
     classifier.fit(train_X, y_train)
     y_pred = classifier.predict(test_X)
-    print(y_test)
-    print(y_pred)
-    # print(f"Accuracy: {accuracy_score(y_pred, y_test)}")
+    print(f"Accuracy from self-made model: {accuracy_score(y_pred, y_test)}")
+
+    model = KNeighborsClassifier(k)
+    model.fit(train_X, y_train)
+    y_preds = model.predict(test_X)
+    print(f"Accuracy from sklearn model: {accuracy_score(y_preds, y_test)}")
+
+
+
+    print("Testing on diabetes data....")
+    df = load_diabetes()
+    regressor = KNNRegression(k)
+    train_X, test_X, y_train, y_test = train_test_split(df.data, df.target, shuffle=True, random_state=4848, test_size=t_size)
+    regressor.fit(train_X, y_train)
+    y_pred = regressor.predict(test_X)
+    print(f"RMSE from self-made model: {mean_squared_error(y_pred, y_test) ** 0.5}")
+    print(f"MAPE from self-made model: {mean_absolute_percentage_error(y_pred, y_test)}")
+
+    model = KNeighborsRegressor(k)
+    model.fit(train_X, y_train)
+    y_preds = model.predict(test_X)
+    print(f"RMSE from sklearn model: {mean_squared_error(y_preds, y_test) ** 0.5}")
+    print(f"MAPE from sklearn model: {mean_absolute_percentage_error(y_preds, y_test)}")
+
+if __name__ == "__main__":
+    main()
